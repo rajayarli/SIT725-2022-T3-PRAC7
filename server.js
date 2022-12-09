f3c161f9a71db14dddcccc804be446987274801a
raja ramesh <62141897+rajayarli@users.noreply.github.com>
2022-12-09 12:08:26 +1100

    Merge branch 'main' of https://github.com/rajayarli/SIT725-2022-T3-PRAC4

--- server.js
+++ server.js
@@ -1,25 +1,86 @@
 var express = require("express");
 var app = express();
-var port = process.env.port || 3000;
+var cors = require("cors");
+let projectCollection;
 
 app.use(express.static(__dirname + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 
-const addNumbers = (number1, number2) => {
-  let num1 = parseInt(number1);
-  let num2 = parseInt(number2);
-  let result = num1 + num2;
-  return result;
+//mongoDb connection
+const MongoClient = require("mongodb").MongoClient;
+const uri ="mongodb+srv://user:pass@cluster0.cqronfo.mongodb.net/?retryWrites=true&w=majority"
+
+const client = new MongoClient(uri, { useNewUrlParser: true });
+
+const createCollection = (collectionName) => {
+  client.connect((err, db) => {
+    projectCollection = client.db().collection(collectionName);
+    if (!err) {
+      console.log("MongoDB Connected");
+    } else {
+      console.log("DB Error: ", err);
+      process.exit(1);
+    }
+  });
 };
 
-app.get("/addTwoNumbers", (req, res) => {
-  let number1 = req.query.number1;
-  let number2 = req.query.number2;
-  let result = addNumbers(number1, number2);
-  res.json({ statusCode: 200, data: result, message: "Success" });
+//insert project
+const insertProjects = (project, callback) => {
+  projectCollection.insert(project, callback);
+};
+
+//post api...
+app.post("/api/projects", (req, res) => {
+  console.log("New Project added", req.body);
+  var newProject = req.body;
+  insertProjects(newProject, (err, result) => {
+    if (err) {
+      res.json({ statusCode: 400, message: err });
+    } else {
+      res.json({
+        statusCode: 200,
+        message: "Project Successfully added",
+        data: result,
+      });
+    }
+  });
+});
+// const cardList = [
+//   {
+//       title: "kangaroo 2",
+//       image: "images/kangaroo2.jpeg",
+//       link: "About kangaroo 2",
+//       desciption: "Demo desciption about kangaroo 2"
+//   },
+//   {
+//       title: "kangaroo 3",
+//       image: "images/kangaroo3.jpeg",
+//       link: "About kangaroo 3",
+//       desciption: "Demo desciption about kangaroo 3"
+//   }
+// ]
+
+//get project...
+const getProjects = (callback) => {
+  projectCollection.find({}).toArray(callback);
+};
+
+app.get("/api/projects", (req, res) => {
+  getProjects((err, result) => {
+    if (err) {
+      res.json({ statusCode: 400, message: err });
+    } else {
+      res.json({ statusCode: 200, message: "Success", data: result });
+    }
+  });
 });
 
+var port = process.env.port || 3000;
+
 app.listen(port, () => {
-  console.log("App listening to: " + port);
+  console.log("App listening to: http://localhost:" + port);
+  createCollection("kangaroos");
 });
+
