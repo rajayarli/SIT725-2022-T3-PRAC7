56ba2fb86f26f9c3e4c7d9521033c686774f4eb8
raja ramesh <62141897+rajayarli@users.noreply.github.com>
2023-01-02 16:02:41 +1100

    final test

--- server.js
+++ server.js
@@ -76,6 +76,18 @@ app.get("/api/projects", (req, res) => {
     }
   });
 });
+app.get('/addTwoNumbers/:firstNumber/:secondNumber', function(req,res,next){
+  var firstNumber = parseInt(req.params.firstNumber) 
+  var secondNumber = parseInt(req.params.secondNumber)
+  var result = firstNumber + secondNumber || null
+  if(result == null) {
+    res.json({result: result, statusCode: 400}).status(400)
+  }
+  else { res.json({result: result, statusCode: 200}).status(200) } 
+})
+
+
+
 
 var port = process.env.port || 3000;
 
@@ -83,4 +95,5 @@ app.listen(port, () => {
   console.log("App listening to: http://localhost:" + port);
   createCollection("kangaroos");
 });
+
 
